4aed8d3a80dcc798eb2d4abedc84f41b6810b15f
Rubel <mr.adcadre41@gmail.com>
2026-03-30 19:47:25 +0600

    width modified

--- video.js
+++ video.js
@@ -19,11 +19,27 @@ function displayCategoryBtn(item) {
 loadCategory();
 
 const details = {
-  category_id: "1001",
-  video_id: "aaaa",
-  thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
-  title: "Shape of You",
-  authors: Array(1),
+  category: [
+    {
+      category_id: "1001",
+      video_id: "aaaa",
+      thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+      title: "Shape of You",
+      authors: [
+        {
+          profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+          profile_name: "Olivia Mitchell",
+          verified: "",
+        },
+      ],
+      others: {
+        views: "100K",
+        posted_date: "16278",
+      },
+      description:
+        "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
+    },
+  ],
 };
 
 const loadVideos = () => {
